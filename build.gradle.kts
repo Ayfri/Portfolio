8e4952c939f451841578cee1b5d1e4bcf5d781db
Ayfri <pierre.ayfri@gmail.com>
2022-06-30 01:30:19 +0200

    Initial commit.

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+
+import org.jetbrains.compose.compose
+
+plugins {
+	kotlin("multiplatform")
+	id("org.jetbrains.compose")
+}
+
+group "fr.ayfri"
+version "1.0-SNAPSHOT"
+
+repositories {
+	google()
+	mavenCentral()
+	maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+}
+
+kotlin {
+	js(IR) {
+		browser {
+			testTask {
+				testLogging.showStandardStreams = true
+				useKarma {
+					useChromeHeadless()
+					useFirefox()
+				}
+			}
+		}
+		binaries.executable()
+	}
+	sourceSets {
+		val jsMain by getting {
+			dependencies {
+				implementation(compose.web.core)
+				implementation(compose.runtime)
+			}
+		}
+		val jsTest by getting {
+			dependencies {
+				implementation(kotlin("test-js"))
+			}
+		}
+	}
+}
