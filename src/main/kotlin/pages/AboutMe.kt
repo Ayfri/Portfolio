3df4ae5414aa24e896105dd10704367a7f396164
Ayfri <pierre.ayfri@gmail.com>
2022-08-15 16:01:40 +0200

    fix: Fix some typos.

--- src/main/kotlin/pages/AboutMe.kt
+++ src/main/kotlin/pages/AboutMe.kt
@@ -25,15 +25,7 @@ import org.w3c.dom.HTMLElement
 import org.w3c.dom.events.Event
 import style.AppStyle
 import style.CSSVariables
-import style.utils.Cursor
-import style.utils.boxShadow
-import style.utils.cursor
-import style.utils.linearGradient
-import style.utils.minContent
-import style.utils.size
-import style.utils.transitions
-import style.utils.width
-import style.utils.zIndex
+import style.utils.*
 
 data class Section(val content: String, val date: Int, val image: Boolean = false, val id: String, val title: (@Composable () -> Unit)? = null) {
 	var htmlElement: HTMLElement? = null
@@ -80,9 +72,9 @@ val sections = listOf(
 	Section(
 		"""
 			First started programming in 2014 by randomly watching a conference about programming in a library.
-			I first tried some experiments in Python with a [book](https://www.eyrolles.com/Informatique/Livre/python-pour-les-kids-9782212140880) that my dad paid me, done some beginner projects etc
+			I first tried some experiments in Python with a [book](https://www.eyrolles.com/Informatique/Livre/python-pour-les-kids-9782212140880) that my dad paid me, done some beginner projects, etc.
 
-			After that, created some random projects I found on Youtube about like programming a Minecraft-Like game in C++ or making some little games in Unity.
+			After that, created some random projects I found on YouTube about like programming a Minecraft-Like game in C++ or making some little games in Unity.
 			But I was not understanding a lot what I was doing and was almost only copying the tutorials without trying myself to create things.
 		""".trimIndent(), 2014, id = "intro"
 	) {
@@ -94,7 +86,7 @@ val sections = listOf(
 			After creating my own [Discord server](https://discord.gg/BySjRNQ9Je) for my community from [Youtube](https://www.youtube.com/c/Ayfri), I wanted to create a Discord Bot. So I created a bot in [JavaScript](https://developer.mozilla.org/docs/Web/JavaScript) using [Node.JS](https://nodejs.org) in early 2018 by following tutorials, I created CommunAyBot.
 			Back in the days it was not that common to create a bot for your own community and Discord was not so reputed. Because of that, there was no really great tutorials and I got a lot of help from a Discord Server named Obelia Dev _(which doesn't even exist today)_, my friends Ghom, Loockeeer, Felons and some others helped me a lot to understand and create my bot.
 
-			After about a year I was pretty good at creating a bot, but I realised that my project was kinda... ugly. So I recreated it, [AyBot 2](https://github.com/Ayfri/AyBot-2) was born in early 2019. It was pretty clean, and I wanted people to use my bot so I changed a lot of the code so it was working on multiple servers with configurations etc.
+			After about a year I was pretty good at creating a bot, but I realised that my project was kinda... ugly. So I recreated it, [AyBot 2](https://github.com/Ayfri/AyBot-2) was born in early 2019. It was pretty clean, and I wanted people to use my bot, so I changed a lot of the code, so it was working on multiple servers with configurations etc.
 			
 			At this time I created some other little utilities bots or test bots.
 			AyBot 2 was taken down in end of 2019 because of [Galileo](#galileo).
@@ -105,12 +97,12 @@ val sections = listOf(
 	
 	Section(
 		"""
-			At my high-school, for 3 years, I used arduino a lot, only creating big projects on the last year but still. It has let me learn a lot on low-level programming, [C++](https://cplusplus.com) basics and micro-controllers.
+			At my high-school, for 3 years, I used arduino a lot, only creating big projects on the last year but still. It has let me learn a lot on low-level programming, [C++](https://cplusplus.com) basics and microcontrollers.
 			By doing my own researches I learned a lot about C++ compilation, assembler, reverse engineering, optimisation and other low-level subjects.
 			I even tried a bit writing assembler x86. I created some little programs using C++.
 			
 			Back in 2015 I followed a [tutorial to create a Minecraft-Like game](https://www.youtube.com/watch?v=GACpZp8oquU) in C++ using [OpenGL](https://www.opengl.org/) and a bunch of libraries.
-			It was interesting to go after some years, back on this project and fix some of the issues and finally understand the code and learn more about OpenGL and low-level graphics processing.
+			It was interesting to go after some years, back on this project and fix some issues and finally understand the code and learn more about OpenGL and low-level graphics processing.
 
 			I currently still have my own arduino that I bought during my high-school.
 		""".trimIndent(), 2018, id = "arduino"
@@ -121,7 +113,7 @@ val sections = listOf(
 	Section(
 		"""
 			At this time I was administrator (and I’m still) on a [big Discord server](https://discord.gg/sDT7W8mNmq) at the time about sharing your server or website. And I was friend with the other administrator, Antow, which also created his own bot, I was helping him sometimes.
-			After some discussions, we accorded to merge our bots together, as we were both passionated by astronomy, we named it [Galileo](https://github.com/Galileo-Bot/galileo).
+			After some discussions, we accorded to merge our bots together, as we were both passionate about astronomy, we named it [Galileo](https://github.com/Galileo-Bot/galileo).
 
 			But these times were complicated for me personally and due to a lack of time and motivations because I was making almost only discord bots since about 2 years. I stopped developing it and moved away to other interesting projects.
 		""".trimIndent(), 2019, id = "galileo"
@@ -133,7 +125,7 @@ val sections = listOf(
 		"""
 			In 2015, I created my first graphic programs using [Processing](https://processing.org) and [Java](https://www.java.com).
 			I heard about this program from watching videos from [The Coding Train](https://www.youtube.com/c/TheCodingTrain). I created a lot of little experiments using it and learned a lot of interesting concepts.
-			I learned some interesting things about 3D graphics, how it works, how to optimise it etc, my biggest 3D expriment was a [Minecraft-Like](https://github.com/Ayfri/ProceCraft) game and my greatest program with Processing was a program to convert an [image to a Minecraft Pixel-Art](https://github.com/Ayfri/Image2Minecraft) using only blocks from Minecraft as pixels.
+			I learned some interesting things about 3D graphics, how it works, how to optimise it etc. My biggest 3D expriment was a [Minecraft-Like](https://github.com/Ayfri/ProceCraft) game and my greatest program with Processing was a program to convert an [image to a Minecraft Pixel-Art](https://github.com/Ayfri/Image2Minecraft) using only blocks from Minecraft as pixels.
 
 			After some years and making some Discord Bots in [JavaScript](https://developer.mozilla.org/docs/Web/JavaScript), I heard that a library in JavaScript existed that was exactly like Processing, [p5.js](https://p5js.org) _(which was created by the same foundation)_, and used it for some little projects, even created a [port](https://github.com/Ayfri/TypeCraft) of the Minecraft-Like game in [TypeScript](https://www.typescriptlang.org).
 			But it was too simple and not enough expandable for me. So I searched another library for making my own games with more depth and customisation.
@@ -148,11 +140,11 @@ val sections = listOf(
 	Section(
 		"""
 			[Minecraft](https://minecraft.net) is my favorite game of all time, I started playing it back in 2010, I know it by heart.
-			Heard about it randomly on an old computer blog that my dad was subscribed to where on an article they spoked about "this new game with an interesting concept".
+			Heard about it randomly on an old computer blog that my dad was subscribed to where on an article they spoked about _"this new game with an interesting concept"_.
 
-			I played hundred of hours on Minecraft, created numerous maps, technical maps using CommandBlocks, Datapacks, mods, even created mods since around 2018. In [Java](https://www.java.com) using [Forge](https://files.minecraftforge.net/net/minecraftforge/forge), [Fabric](https://fabricmc.net) since 2020 and in [Kotlin](https://kotlinlang.org) since 2021.
+			I played a hundred of hours on Minecraft, created numerous maps, technical maps using CommandBlocks, Datapacks, mods, even created mods since around 2018. In [Java](https://www.java.com) using [Forge](https://files.minecraftforge.net/net/minecraftforge/forge), [Fabric](https://fabricmc.net) since 2020 and in [Kotlin](https://kotlinlang.org) since 2021.
 
-			I know a lot about the the concepts of Minecraft and how they are programmed, this is what motivated me to create a Minecraft-Like game, _4 times_. Approaching more what I want and performances needed for a game like this each time.
+			I know a lot about the concepts of Minecraft and how they are programmed, this is what motivated me to create a Minecraft-Like game, _4 times_. Approaching more what I want and performances needed for a game like this each time.
 		""".trimIndent(), 2018, id = "minecraft"
 	) {
 		TextIcon("Minecraft", localImage("minecraft.svg"))
@@ -162,11 +154,11 @@ val sections = listOf(
 		"""
 			After getting graduated from high-school, I got accepted into a private school named [Ynov](https://ynov.com).
 			We learned so far [GoLang](https://go.dev), [Python](https://www.python.org), pretty complex [C++](https://cplusplus.com) and POO into C++, some network basics, how to use [REST API](https://wikipedia.org/wiki/Representational_state_transfer)s, the team JS/HTML/CSS, databases and [MySQL](https://www.mysql.com), and other useful technologies.
-			I was already knowing some but I appreciated a lot GoLang’s GoHTML Templates, C++ and my teacher who I chatted a lot with and understanding a lot more how network work.
+			I was already knowing some, but I appreciated a lot GoLang’s GoHTML Templates, C++ and my teacher who I chatted a lot with and understanding a lot more how network work.
 	
 			We created a bunch of projects, starting with little training projects in GoLang/Python/C++/JavaScript, done some websites using only front, but also using an API, and using MySQL creating an entire forum.
 	
-			It is for me a great experience being in this school and I’m excited for the next 4 years !
+			It is for me a great experience being in this school, and I’m excited for the next 4 years !
 		""".trimIndent(), 2021, id = "ynov"
 	) {
 		TextIcon("Post-Bac and Ynov", "https://www.ynov.com/wp-content/themes/ynov/assets/images/favicons/apple-touch-icon.png")
@@ -177,7 +169,7 @@ val sections = listOf(
 		My school required me to have an internship of 6 weeks in any enterprise to have a first professional experience.
 		I got in contact with [BlueFrog](https://www.bluefrog.fr/index.html), a company creating websites, and after some interviews got accepted !
 
-		I learned [PHP](https://www.php.net) and [WordPress](https://wordpress.org) and created a few websites during my traineeship and it was a great experience for me.
+		I learned [PHP](https://www.php.net) and [WordPress](https://wordpress.org) and created a few websites during my traineeship, and it was a great experience for me.
 		Learning a bunch of useful technologies, the difference between personal project and real projects and a view of what is the job of developer.
 	""".trimIndent(), 2022, id = "first-internship"
 	) {
@@ -285,7 +277,7 @@ object AboutMeStyle : StyleSheet() {
 		}
 		
 		id("main") style {
-			timelineSize(12.vw)
+			timelineSize(max(8.cssRem, 10.vw))
 			padding(0.px)
 		}
 	}
@@ -314,6 +306,7 @@ object AboutMeStyle : StyleSheet() {
 		alignItems(AlignItems.Center)
 		
 		position(Position.Absolute)
+		left(1.5.cssRem)
 		height(90.percent)
 		width(timelineSize.value())
 		
@@ -384,6 +377,10 @@ object AboutMeStyle : StyleSheet() {
 		flexDirection(FlexDirection.Row)
 		alignItems(AlignItems.Center)
 		gap(2.5.cssRem)
+		
+		media(mediaMaxWidth(768.px)) {
+			flexDirection(FlexDirection.Column)
+		}
 	}
 	
 	val content by style {
@@ -438,5 +435,10 @@ object AboutMeStyle : StyleSheet() {
 		justifyContent(JustifyContent.Start)
 		flexDirection(FlexDirection.Row)
 		gap(1.5.cssRem)
+		
+		media(mediaMaxWidth(520.px)) {
+			flexDirection(FlexDirection.Column)
+			textAlign(TextAlign.Center)
+		}
 	}
 }
