8610c1524f93d21d2d2494843a1f577f4f669e14
Ayfri <pierre.ayfri@gmail.com>
2022-08-21 18:24:18 +0200

    refactor: Put Meta tags into HTML directly so they work.

--- src/main/kotlin/Head.kt
+++ src/main/kotlin/Head.kt
@@ -3,61 +3,15 @@ import kotlinx.browser.document
 import org.jetbrains.compose.web.css.Style
 import org.jetbrains.compose.web.dom.TagElement
 import org.w3c.dom.HTMLLinkElement
-import org.w3c.dom.HTMLMetaElement
 import org.w3c.dom.HTMLScriptElement
 import style.AppStyle
 
-private const val description = "The portfolio of myself, Pierre Roy."
-
 @Composable
 fun Head() {
-	Meta(charset = "utf-8")
-	MetaName(name = "viewport", content = "width=device-width, initial-scale=1")
-	MetaName(name = "description", content = description)
-	MetaName(name = "author", content = "Ayfri")
-	
-	MetaName(name = "twitter:card", content = "summary")
-	MetaName(name = "twitter:site", content = "@Ayfri_")
-	MetaName(name = "twitter:creator", content = "@ayfri_")
-	MetaName(name = "twitter:title", content = "Ayfri Portfolio")
-	MetaName(name = "twitter:description", content = description)
-	
-	MetaProperty("og:title", content = "Ayfri Portfolio")
-	MetaProperty("og:description", content = description)
-	MetaProperty("og:type", content = "website")
-	MetaProperty("og:image", content = localImage("avatar.jpg"))
-	
-	document.location?.href?.let {
-		MetaProperty("og:url", content = it)
-	}
-	
 	Style(AppStyle)
 	Link(href = "https://fonts.googleapis.com/css2?family=Open+Sans:wght@100;300;400;500;700&display=swap", rel = "stylesheet")
 	Link(href = "https://dev-cats.github.io/code-snippets/JetBrainsMono.css", rel = "stylesheet")
 	Script(src = "https://kit.fontawesome.com/74fed0e2b5.js", crossOrigin = CrossOrigin.ANONYMOUS)
-}
-
-@Composable
-fun MetaName(name: String, content: String) {
-	TagElement<HTMLMetaElement>("meta", {
-		attr("name", name)
-		attr("content", content)
-	}) {}
-}
-
-@Composable
-fun MetaProperty(property: String, content: String) {
-	TagElement<HTMLMetaElement>("meta", {
-		attr("property", property)
-		attr("content", content)
-	}) {}
-}
-
-@Composable
-fun Meta(charset: String) {
-	TagElement<HTMLMetaElement>("meta", {
-		attr("charset", charset)
-	}, {})
 }
 
 @Composable
