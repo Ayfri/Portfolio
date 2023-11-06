ee212f6097e91c5eb5d6849e0af62c803a30f007
Ayfri <pierre.ayfri@gmail.com>
2023-11-06 22:38:19 +0100

    feat(meta): Add metadata tags.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,6 @@
 import com.varabyte.kobweb.gradle.application.util.configAsKobwebApplication
+import kotlinx.html.HEAD
+import kotlinx.html.meta
 import org.jetbrains.kotlin.gradle.dsl.KotlinJsCompile
 import org.jetbrains.kotlin.gradle.targets.js.dsl.KotlinJsBinaryMode
 import org.jetbrains.kotlin.gradle.targets.js.ir.JsIrBinary
@@ -22,10 +24,45 @@ repositories {
 	maven("https://us-central1-maven.pkg.dev/varabyte-repos/public")
 }
 
+fun HEAD.meta(property: String, content: String) {
+	meta {
+		attributes["property"] = property
+		this.content = content
+	}
+}
+
 kobweb {
 	app {
 		index {
-			description.set("Powered by Kobweb")
+			val url = "https://ayfri.com"
+			val author = "Pierre Roy"
+			val twitterHandle = "@Ayfri_"
+			val description = "Portfolio of Pierre Roy, student at Ynov and developer."
+			val image = "$url/images/avatar.webp"
+			val title = "$author - Portfolio"
+
+			this.description = description
+
+			head.apply {
+				add {
+					meta(charset = "utf-8")
+					meta(name = "viewport", content = "width=device-width, initial-scale=1.0")
+					meta(name = "Author", content = author)
+
+					meta(property = "og:description", content = description)
+					meta(property = "og:image", content = image)
+					meta(property = "og:title", content = title)
+					meta(property = "og:type", content = "website")
+					meta(property = "og:url", content = url)
+
+					meta(property = "twitter:card", content = "summary")
+					meta(property = "twitter:creator", content = twitterHandle)
+					meta(property = "twitter:description", content = description)
+					meta(property = "twitter:image", content = image)
+					meta(property = "twitter:title", content = title)
+					meta(property = "twitter:site", content = twitterHandle)
+				}
+			}
 		}
 	}
 }
