e455bdc52dd6012043b54af45ded05a0be8c8f13
Ayfri <pierre.ayfri@gmail.com>
2023-12-03 11:42:38 +0100

    feat(meta): Add canonical URL to main pages.

--- src/jsMain/kotlin/io/github/ayfri/layouts/PageLayout.kt
+++ src/jsMain/kotlin/io/github/ayfri/layouts/PageLayout.kt
@@ -1,9 +1,11 @@
 package io.github.ayfri.layouts
 
 import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.core.rememberPageContext
 import io.github.ayfri.AppStyle
 import io.github.ayfri.Footer
 import io.github.ayfri.header.Header
+import io.github.ayfri.setCanonical
 import io.github.ayfri.setTitle
 import kotlinx.browser.window
 import org.jetbrains.compose.web.css.Style
@@ -15,6 +17,12 @@ fun PageLayout(title: String, content: @Composable () -> Unit) {
 	setTitle("$title - Pierre Roy")
 
 	Header()
+
+	val context = rememberPageContext()
+	val currentStub = context.route
+	var canonicalUrl = "https://ayfri.com$currentStub"
+	if (!canonicalUrl.endsWith("/")) canonicalUrl += "/"
+	setCanonical(canonicalUrl)
 
 	Main({
 		id("main")
