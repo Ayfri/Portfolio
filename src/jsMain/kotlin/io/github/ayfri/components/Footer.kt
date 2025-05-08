dd09d80282ddf9db1b527786c3b673fa3bdbc3cb
Ayfri <pierre.ayfri@gmail.com>
2025-05-09 00:55:18 +0200

    feat(footer): Update project link from Pokestore to PokeCards-Collector.

--- src/jsMain/kotlin/io/github/ayfri/components/Footer.kt
+++ src/jsMain/kotlin/io/github/ayfri/components/Footer.kt
@@ -37,7 +37,7 @@ data class ProjectLink(val name: String, val url: String)
 val projectLinks = listOf(
 	ProjectLink("Kore", "https://kore.ayfri.com"),
 	ProjectLink("Atom Clicker", "https://atom-clicker.ayfri.com"),
-	ProjectLink("Pokestore", "https://pokestore.ayfri.com"),
+	ProjectLink("PokeCards-Collector", "https://pokecards-collector.ayfri.com"),
 	ProjectLink("Cursors Draw", "https://cursors.draw.ayfri.com"),
 	ProjectLink("Realtime TodoList", "https://realtime-todolist.pages.dev"),
 	ProjectLink("GPT Images", "https://gpt-images.ayfri.com"),
