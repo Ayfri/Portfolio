5529c5aaadea20752345dba7acdaf846c53a234d
Ayfri <pierre.ayfri@gmail.com>
2025-11-04 02:00:03 +0100

    refactor(links): Add GitHub profile link and reorder repository link for clarity.

--- src/jsMain/kotlin/io/github/ayfri/data/links.kt
+++ src/jsMain/kotlin/io/github/ayfri/data/links.kt
@@ -1,7 +1,8 @@
 package io.github.ayfri.data
 
-const val REPO_LINK = "https://github.com/Ayfri/Portfolio"
 const val DISCORD_LINK = "https://discord.gg/invite/BySjRNQ9Je"
+const val GITHUB_PROFILE = "https://github.com/Ayfri"
 const val LINKEDIN_LINK = "https://www.linkedin.com/in/pierre-roy-ayfri/"
+const val REPO_LINK = "https://github.com/Ayfri/Portfolio"
 const val TWITCH_LINK = "https://www.twitch.tv/ayfri_"
 const val TWITTER_LINK = "https://twitter.com/@ayfri_"
